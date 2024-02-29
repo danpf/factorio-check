0c9033b03ffd8dc4d8d142966c84c9f55c9239cc
Danny Farrell <16297104+danpf@users.noreply.github.com>
2024-02-29 09:51:10 -0500

    updatee entrypoint code

--- docker/files/lint-entrypoint.sh
+++ docker/files/lint-entrypoint.sh
@@ -13,9 +13,10 @@ if [ ! -d "$TARGET_DIR" ]; then
 	exit 1
 fi
 
-lua-language-server --check "$TARGET_DIR" --configpath /opt/factorio/luarc.json > /dev/null
+lua-language-server --check "$TARGET_DIR" --configpath /opt/factorio/luarc.json || echo "lua-language-server command failed" && exit 1
 CHECK_FILE="/opt/luals/lua-language-server/log/check.json"
 if [ -f $CHECK_FILE ]; then
+	echo "Linting complete: Errors found!"
 	jq -r '. as $root | to_entries[] | .key as $filename | .value | unique[] | "\($filename) code: \(.code), message: \(.message), severity: \(.severity), source: \(.source), line:char-range: \(.range.start.line):\(.range.start.character)-\(.range.end.line):\(.range.end.character)"' $CHECK_FILE
 	exit 1
 fi
